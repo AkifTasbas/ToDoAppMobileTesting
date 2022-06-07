7859e803fa680b0e88e97d2179092164a9f2dedf
akiftasbas <32tasbasakif32@gmail.com>
2022-06-07 18:17:52 +0200

    BackgroundColor Screen Shots Added

--- src/test/java/check24/step_definitions/Aufgabe5StepDefs.java
+++ src/test/java/check24/step_definitions/Aufgabe5StepDefs.java
@@ -4,8 +4,13 @@ import check24.utilities.Driver;
 import io.appium.java_client.MobileElement;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class Aufgabe5StepDefs {
@@ -17,26 +22,27 @@ public class Aufgabe5StepDefs {
     }
 
     @Then("the background should be dark")
-    public void the_background_should_be_dark() throws MalformedURLException, InterruptedException {
-      /*  System.out.println("Background is dark");
-        MobileElement background = Driver.getDriver().findElement(By.cssSelector("body[background-color='white']"));
-        String cssProperty = background.getCssValue("background-color");
-        System.out.println("cssProperty = " + cssProperty);*/
+    public void the_background_should_be_dark() throws IOException, InterruptedException {
+        Thread.sleep(3000);
+        File file  = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("darkBackground.jpg"));
 
 
     }
 
     @Then("User clicks DayNight mode again")
     public void user_clicks_DayNight_mode_again() throws MalformedURLException, InterruptedException {
-        Thread.sleep(3000);
+        //Thread.sleep(3000);
         MobileElement mondIcon = Driver.getDriver().findElement(By.id("com.example.yeshasprabhakar.todo:id/themeActionButton"));
-        Thread.sleep(3000);
+        //Thread.sleep(3000);
         mondIcon.click();
     }
 
     @Then("the background should be light")
-    public void the_background_should_be_light() {
-        System.out.println("Background is light");
+    public void the_background_should_be_light() throws IOException, InterruptedException {
+        Thread.sleep(3000);
+        File file  = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("lightBackground.jpg"));
     }
 
 }
